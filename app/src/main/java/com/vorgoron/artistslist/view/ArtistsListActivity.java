1cd7f948a8d078b88cab570ebd1e29f556680a9e
Grigory Grigoryev <ggrigoriev@65apps.com>
2016-04-11 12:55:19 +0400

    Add item divider

--- app/src/main/java/com/vorgoron/artistslist/view/ArtistsListActivity.java
+++ app/src/main/java/com/vorgoron/artistslist/view/ArtistsListActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.widget.RecyclerView;
 
 import com.vorgoron.artistslist.R;
 import com.vorgoron.artistslist.adapter.ArtistAdapter;
+import com.vorgoron.artistslist.adapter.SimpleDividerItemDecoration;
 import com.vorgoron.artistslist.model.api.response.Artist;
 import com.vorgoron.artistslist.presenter.ArtistsListPresenter;
 
@@ -32,6 +33,7 @@ public class ArtistsListActivity extends BaseActivity<ArtistsListPresenter> {
     public void setArtists(List<Artist> artists) {
         ArtistAdapter artistAdapter = new ArtistAdapter(this, artists);
         list.setLayoutManager(new LinearLayoutManager(this));
+        list.addItemDecoration(new SimpleDividerItemDecoration(this));
         list.setAdapter(artistAdapter);
         hideProgress();
     }
